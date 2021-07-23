51127683b1b89238d028bcb7fa7dd8c052c88700
hit9 <hit9@icloud.com>
2021-07-24 00:49:53 +0800

    Using realpath instead of cd and out

--- env.fish
+++ env.fish
@@ -1,3 +1,3 @@
-set -x OO_ROOT_DIR (cd (dirname (status -f)); and pwd -P)
+set -x OO_ROOT_DIR (realpath (dirname (status -f)))
 set -x GOROOT $OO_ROOT_DIR/go
 set -x fish_user_paths $OO_ROOT_DIR/bin $GOROOT/bin $fish_user_paths
